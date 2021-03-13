1fb819032190253d1e6e6ace48f9896cd3e560b0
jamieroszel <jamie.s.roszel@gmail.com>
2021-03-13 14:32:51 -0500

    add time function to server.js

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
+//************************** */
+// IMPORTING DEPENDENCIES
+//************************** */
+
 // import express
 const express = require("express")
+
 //import the route
 const indexRouter = require("./routes/index")
 const todosRouter = require("./routes/todos")
 
-// get request to /todos/cheese
+
+//************************** */
+// Create Application Object
+//************************** */
 
 // generate app object
 const app = express()
 
+//************************** */
+// Set View Engine
+//************************** */
+
 //configure ejs
 app.set("view engine", "ejs")
 
+//************************** */
+// Middleware
+//************************** */
+//************************** */
+app.use((req, rest, next) => {
+  console.log('Hello, intrepid learner')
+  // add a time property to the request object
+  req.time = new Date().toLocaleTimeString()
+  console.log(req.time)
+  // go to the next middleware function
+  next()
+})
+
+
+// Routing
+//************************** */
 //routes
 app.use("/", indexRouter)
 app.use("/todos", todosRouter)
-
-
-
+//************************** */
+// Listener
+//************************** */
 // tell the app to listen on port 3000
 app.listen(3000, () => console.log("listening on port 3000"))
